5ec9dc6ea86fde3dc0a2554154fa7f56cd4ff26a
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-03-30 05:55:45 +0800

    Fix: swap enabled and disabled

--- station/lib/parameters.py
+++ station/lib/parameters.py
@@ -2,20 +2,20 @@ from agrf.parameters import Parameter, ParameterList
 from agrf.magic import Switch
 from grf import ParameterMapping
 
-booldict = {0: "ENABLED", 1: "DISABLED"}
+booldict = {0: "DISABLED", 1: "ENABLED"}
 
 parameter_list = ParameterList(
     [
         Parameter(
             "RAIL_STATION",
-            0,
+            1,
             booldict,
             mapping=ParameterMapping(grf_parameter=0x0, first_bit=0, num_bit=1),
             option_name="BOOLEAN",
         ),
         Parameter(
             "ROADSTOP",
-            0,
+            1,
             booldict,
             mapping=ParameterMapping(grf_parameter=0x0, first_bit=1, num_bit=1),
             option_name="BOOLEAN",
