51bc3f0870005844372a987ab22fcbd9920d886e
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-03-30 03:44:11 +0800

    Refactor: change demo image path

--- station/lib/docgen.py
+++ station/lib/docgen.py
@@ -115,7 +115,7 @@ changelog""",
         single_section = len(demos_dict) == 1
         for title, demos_list in demos_dict.items():
             demok = title.replace(" ", "_").lower()
-            os.makedirs(os.path.join(prefix, "img", metastation_label, "layouts", demok), exist_ok=True)
+            os.makedirs(os.path.join(prefix, "img", metastation_label, "demos", demok), exist_ok=True)
             tocentry = f"{metastation_label}_{demok}"
             demo_toc.append(tocentry)
             with open(os.path.join(prefix, f"{tocentry}.rst"), "w") as f:
@@ -123,14 +123,14 @@ changelog""",
                     print(f"================\n{title}\n================\n", file=f)
                 for i, demo in enumerate(demos_list):
                     img = demo.graphics(4, 32).crop().resize(1920, 1080).to_pil_image()
-                    img.save(os.path.join(prefix, "img", f"{metastation_label}/layouts/{demok}/{i:04X}.png"))
+                    img.save(os.path.join(prefix, "img", f"{metastation_label}/demos/{demok}/{i:04X}.png"))
                     print(
                         f"""
 ----------------
 {demo.title}
 ----------------
 
-.. image:: img/{metastation_label}/layouts/{demok}/{i:04X}.png
+.. image:: img/{metastation_label}/demos/{demok}/{i:04X}.png
 """,
                         file=f,
                     )
