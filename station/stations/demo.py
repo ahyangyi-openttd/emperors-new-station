e3c4ec967f6bb51d468f3ea11a11f6c9386314a4
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-03-30 02:57:44 +0800

    Docs: roadstop demo

--- station/stations/demo.py
+++ station/stations/demo.py
@@ -1,4 +1,4 @@
-from station.lib import Demo
+from station.lib import Demo, ALayout, ADefaultGroundSprite
 
 
 def repeat(layouts_list, n):
@@ -6,8 +6,34 @@ def repeat(layouts_list, n):
 
 
 def get_demos(entries):
+    from .roadstops import layout_0, layout_1, layout_2, layout_3
+
+    road_paved_x = ADefaultGroundSprite(1314)
+    road_paved_y = ADefaultGroundSprite(1313)
+    road_t_junction_e = ADefaultGroundSprite(1316)
+    road_t_junction_n = ADefaultGroundSprite(1317)
+    road_t_junction_w = ADefaultGroundSprite(1318)
+    road_t_junction_s = ADefaultGroundSprite(1319)
+
+    layout_road_x = ALayout(road_paved_x, [], True)
+    layout_road_y = ALayout(road_paved_y, [], True)
+    layout_road_t_junction_e = ALayout(road_t_junction_e, [], True)
+    layout_road_t_junction_n = ALayout(road_t_junction_n, [], True)
+    layout_road_t_junction_w = ALayout(road_t_junction_w, [], True)
+    layout_road_t_junction_s = ALayout(road_t_junction_s, [], True)
+
     return {
         "Sample Layouts": [
-            Demo(repeat([[entries[1]], [entries[0]], [entries[0]], [entries[2]]], 4), "Emperor's New Rail Station")
+            Demo(repeat([[entries[1]], [entries[0]], [entries[0]], [entries[2]]], 4), "Emperor's New Rail Station"),
+            Demo(
+                [
+                    [entries[2], layout_1, entries[2], entries[2], entries[2]],
+                    [entries[2], layout_road_t_junction_e, layout_road_x, layout_road_t_junction_s, layout_2],
+                    [entries[2], layout_road_y, entries[2], layout_road_y, entries[2]],
+                    [layout_0, layout_road_t_junction_n, layout_road_x, layout_road_t_junction_w, entries[2]],
+                    [entries[2], entries[2], entries[2], layout_3, entries[2]],
+                ],
+                "Emperor's New Road Stop",
+            ),
         ]
     }
--- station/stations/roadstops.py
+++ station/stations/roadstops.py
@@ -5,14 +5,15 @@ from station.lib.parameters import parameter_list
 from station.stations.railway_stations import ENS_CLASS
 
 # Bus stop ground sprites for bay stops (views 0-3)
-bus_stop_0 = ADefaultGroundSprite(2692)
-bus_stop_1 = ADefaultGroundSprite(2693)
-bus_stop_2 = ADefaultGroundSprite(2694)
-bus_stop_3 = ADefaultGroundSprite(2695)
+bus_stop_0 = ADefaultGroundSprite(2691)
+bus_stop_1 = ADefaultGroundSprite(2692)
+bus_stop_2 = ADefaultGroundSprite(2693)
+bus_stop_3 = ADefaultGroundSprite(2694)
 
 # Road paved sprites for drive-through (views 4-5)
 road_paved_x = ADefaultGroundSprite(1314)
 road_paved_y = ADefaultGroundSprite(1313)
+
 
 layout_0 = ALayout(bus_stop_0, [], True, category=ENS_CLASS)
 layout_1 = ALayout(bus_stop_1, [], True, category=ENS_CLASS)
@@ -20,6 +21,7 @@ layout_2 = ALayout(bus_stop_2, [], True, category=ENS_CLASS)
 layout_3 = ALayout(bus_stop_3, [], True, category=ENS_CLASS)
 layout_4 = ALayout(road_paved_x, [], True, category=ENS_CLASS)
 layout_5 = ALayout(road_paved_y, [], True, category=ENS_CLASS)
+
 switch = Switch(ranges={0: layout_0, 1: layout_1, 2: layout_2, 3: layout_3, 4: layout_4}, default=layout_5, code="view")
 
 roadstops = [
