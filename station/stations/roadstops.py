4d10d74b78773645b250b88742fc38cd7acf40de
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-03-30 04:15:58 +0800

    Feature: a new road stop

--- station/stations/roadstops.py
+++ station/stations/roadstops.py
@@ -4,44 +4,58 @@ from station.lib import ALayout, ADefaultGroundSprite
 from station.lib.parameters import parameter_list
 from station.stations.railway_stations import ENS_CLASS
 
-# Bus stop ground sprites for bay stops (views 0-3)
-bus_stop_0 = ADefaultGroundSprite(2691)
-bus_stop_1 = ADefaultGroundSprite(2692)
-bus_stop_2 = ADefaultGroundSprite(2693)
-bus_stop_3 = ADefaultGroundSprite(2694)
+stop_0 = ADefaultGroundSprite(2692)
+stop_1 = ADefaultGroundSprite(2693)
+stop_2 = ADefaultGroundSprite(2694)
+stop_3 = ADefaultGroundSprite(2695)
 
-# Road paved sprites for drive-through (views 4-5)
-road_paved_x = ADefaultGroundSprite(1314)
-road_paved_y = ADefaultGroundSprite(1313)
+paved_x = ADefaultGroundSprite(1314)
+paved_y = ADefaultGroundSprite(1313)
+paved_1332_x = ADefaultGroundSprite(1333)
+paved_1332_y = ADefaultGroundSprite(1332)
 
-
-layout_0 = ALayout(bus_stop_0, [], True, category=ENS_CLASS)
-layout_1 = ALayout(bus_stop_1, [], True, category=ENS_CLASS)
-layout_2 = ALayout(bus_stop_2, [], True, category=ENS_CLASS)
-layout_3 = ALayout(bus_stop_3, [], True, category=ENS_CLASS)
-layout_4 = ALayout(road_paved_x, [], True, category=ENS_CLASS)
-layout_5 = ALayout(road_paved_y, [], True, category=ENS_CLASS)
+layout_0 = ALayout(stop_0, [], True, category=ENS_CLASS)
+layout_1 = ALayout(stop_1, [], True, category=ENS_CLASS)
+layout_2 = ALayout(stop_2, [], True, category=ENS_CLASS)
+layout_3 = ALayout(stop_3, [], True, category=ENS_CLASS)
+layout_4 = ALayout(paved_x, [], True, category=ENS_CLASS)
+layout_5 = ALayout(paved_y, [], True, category=ENS_CLASS)
+layout_1332_4 = ALayout(paved_1332_x, [], True, category=ENS_CLASS)
+layout_1332_5 = ALayout(paved_1332_y, [], True, category=ENS_CLASS)
 
 switch = Switch(ranges={0: layout_0, 1: layout_1, 2: layout_2, 3: layout_3, 4: layout_4}, default=layout_5, code="view")
 
-roadstops = [
-    ARoadStop(
-        id=0x00,
-        translation_name="STOP",
-        graphics=switch,
-        general_flags=0x0,
-        class_label=ENS_CLASS,
-        enable_if=[parameter_list["ROADSTOP"]],
-        doc_layout=layout_0,
-    ),
-    ARoadStop(
-        id=0x80,
-        translation_name="WAYPOINT",
-        graphics=switch,
-        general_flags=0x8,
-        class_label=ENS_CLASS,
-        enable_if=[parameter_list["ROADSTOP"]],
-        doc_layout=layout_0,
-        is_waypoint=True,
-    ),
-]
+switch_1332 = Switch(
+    ranges={0: layout_0, 1: layout_1, 2: layout_2, 3: layout_3, 4: layout_1332_4}, default=layout_1332_5, code="view"
+)
+
+
+def make_roadstop(roadstops, base_id, translation_name, graphics, doc_layout, general_flags=0x0):
+    roadstops.append(
+        ARoadStop(
+            id=base_id,
+            translation_name=translation_name,
+            graphics=graphics,
+            general_flags=general_flags,
+            class_label=ENS_CLASS,
+            enable_if=[parameter_list["ROADSTOP"]],
+            doc_layout=doc_layout,
+        )
+    )
+    roadstops.append(
+        ARoadStop(
+            id=0x80 + base_id,
+            translation_name="WAYPOINT",
+            graphics=graphics,
+            general_flags=0x8,
+            class_label=ENS_CLASS,
+            enable_if=[parameter_list["ROADSTOP"]],
+            doc_layout=doc_layout,
+            is_waypoint=True,
+        )
+    )
+
+
+roadstops = []
+make_roadstop(roadstops, 0x00, "STOP", switch, layout_4)
+make_roadstop(roadstops, 0x01, "STOP", switch_1332, layout_1332_4, general_flags=0x8)
