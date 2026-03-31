cea31753b02c9f4a69f1033137bf0348a3fc7c56
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-04-01 03:42:52 +0800

    Fix: last commit doesn't even compile..

--- station/stations/railway_stations.py
+++ station/stations/railway_stations.py
@@ -10,26 +10,23 @@ track_layout = ALayout(default_railroad, [], True)
 ENS_CLASS = b"ENS_"
 
 
-def make_station(station_tiles, entries, layout, symmetry, base_id, notes=None):
-    var = symmetry.get_all_variants(layout)
-    l = symmetry.create_variants(var)
-
+def make_station(station_tiles, entries, layout, base_id, notes=None):
     station_tiles.append(
         AStation(
             id=base_id,
             translation_name="RAIL_NAME",
-            layouts=[l, l.M],
+            layouts=[layout, layout.M],
             class_label=ENS_CLASS,
             non_traversable_tiles=0b00 if layout.traversable else 0b11,
             is_waypoint=False,
             callbacks={"select_tile_layout": 0, "select_sprite_layout": 0},
             enable_if=[parameter_list["RAIL_STATION"]],
-            doc_layout=l,
+            doc_layout=layout,
         )
     )
 
     if layout.traversable:
-        wp_l = replace(l, notes=["waypoint"])
+        wp_l = replace(layout, notes=["waypoint"])
         station_tiles.append(
             AStation(
                 id=0x80 + base_id,
@@ -44,16 +41,16 @@ def make_station(station_tiles, entries, layout, symmetry, base_id, notes=None):
             )
         )
 
-    entries.append(l)
+    entries.append(layout)
 
 
 def register():
     station_tiles = []
     entries = []
 
-    make_station(station_tiles, entries, replace(track_layout, category=ENS_CLASS), BuildingSymmetrical, 0x00)
-    make_station(station_tiles, entries, replace(grassland_tile, category=ENS_CLASS), BuildingCylindrical, 0x10)
-    make_station(station_tiles, entries, replace(building_ground_tile, category=ENS_CLASS), BuildingCylindrical, 0x11)
+    make_station(station_tiles, entries, replace(track_layout, category=ENS_CLASS), 0x00)
+    make_station(station_tiles, entries, replace(grassland_tile, category=ENS_CLASS), 0x10)
+    make_station(station_tiles, entries, replace(building_ground_tile, category=ENS_CLASS), 0x11)
 
     return station_tiles, entries
 
