1c9a23cda35626c18465ef4a5ce7146f6d506a59
Yi Yang @ Asterodeia <ahyangyi@gmail.com>
2026-04-01 13:50:53 +0800

    Refactor: move AStation to agrf

--- station/lib/__init__.py
+++ station/lib/__init__.py
@@ -1,4 +1,4 @@
-from .station import AStation
+from agrf.lib.building.station import AStation
 from agrf.lib.building.symmetry import BuildingSymmetrical, BuildingCylindrical
 from agrf.lib.building.layout import ADefaultGroundSprite, ALayout
 from .metastation import AMetaStation
